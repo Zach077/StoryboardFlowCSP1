325b4bc90891da01fec090247825736f1c2b11db
Zach077 <zachtimkim@gmail.com>
2019-10-17 09:14:49 -0700

    Initial Commit

---
+++ Models/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Sutherland, Zachary on 10/15/19.
+//  Copyright © 2019 Sutherland, Zachary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
